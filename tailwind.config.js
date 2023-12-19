ceab5940945784df2db913c957fd5a4ee0df174c
afnanhussain2002 <afnanhussain2022@gmail.com>
2023-12-19 20:16:58 +0600

    Style: Add a primary color in tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#BD27EA'
+      }
+    },
+    
   },
   plugins: [],
 }
